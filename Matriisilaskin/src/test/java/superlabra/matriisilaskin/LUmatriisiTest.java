2e3c7caeada01d1ed032391af15014869614a4a5
xanxan <anna.emilia.salo@gmail.com>
2015-01-22 22:40:53 +0200

    junit testit

---
+++ Matriisilaskin/src/test/java/superlabra/matriisilaskin/LUmatriisiTest.java
@@ -0,0 +1,47 @@
+package superlabra.matriisilaskin;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author anna
+ */
+public class LUmatriisiTest {
+    private int[][] m1 = {{2,0,-5},
+                         {1,6,100}};
+     private Normimatriisi x;
+   
+    public LUmatriisiTest() {
+         this.x = new Normimatriisi(m1);
+    }
+    
+    
+
+  
+
+    /**
+     * Test of skalaaritulo method, of class LUmatriisi.
+     */
+    @Test
+    public void testSkalaaritulo() {
+        int a = 0; int b = 1; int c = 5; int d = -100;
+       
+       int[][] ma = new int[2][3];
+       int[][] mc = {{10,0,-25},
+                    {5,30,500}};
+       int[][] md = {{-200,0,500},
+                     {-100,-600,-10000}};
+       
+       assertArrayEquals(ma, x.skalaaritulo(a));
+       assertArrayEquals(m1, x.skalaaritulo(b));
+       assertArrayEquals(mc, x.skalaaritulo(c));
+       assertArrayEquals(md, x.skalaaritulo(d));
+    }
+
+    
+}
---
+++ Matriisilaskin/src/test/java/superlabra/matriisilaskin/NormimatriisiTest.java
@@ -0,0 +1,102 @@
+package superlabra.matriisilaskin;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+/**
+ *
+ * @author anna
+ */
+public class NormimatriisiTest {
+    private int[][] m1 = {{2,0,-5},
+                         {1,6,100}};
+    private int[][] m2 = {{5,0,10},
+                         {-4,-3,1}};
+    private int[][] m3 = {{0,1},
+                         {10,-9}};
+   
+    private Normimatriisi x;
+    private Normimatriisi y;
+    private Normimatriisi z;
+    
+    public NormimatriisiTest() {
+        this.x = new Normimatriisi(m1);
+        this.y = new Normimatriisi(m2);
+        this.z = new Normimatriisi(m3);
+    }
+    
+ 
+
+    
+
+    /**
+     * Test of skalaaritulo method, of class Normimatriisi.
+     */
+    @Test
+    public void testSkalaaritulo() {
+       
+       
+       int a = 0; int b = 1; int c = 5; int d = -100;
+       
+       int[][] ma = new int[2][3];
+       int[][] mc = {{10,0,-25},
+                    {5,30,500}};
+       int[][] md = {{-200,0,500},
+                     {-100,-600,-10000}};
+       
+       assertArrayEquals(ma, x.skalaaritulo(a));
+       assertArrayEquals(m1, x.skalaaritulo(b));
+       assertArrayEquals(mc, x.skalaaritulo(c));
+       assertArrayEquals(md, x.skalaaritulo(d));
+       
+
+    }
+
+    /**
+     * Test of matriisiensumma method, of class Normimatriisi.
+     */
+    @Test
+    public void testMatriisiensumma() {
+        
+         int[][] summa = {{7,0,5},
+                         {-3,3,101}};
+         
+         assertArrayEquals(summa, x.matriisiensumma(y));
+         
+         
+         
+    }
+
+    /**
+     * Test of matriisienerotus method, of class Normimatriisi.
+     */
+    @Test
+    public void testMatriisienerotus() {
+          int[][] erotus = {{-3,0,-15},
+                           {5,9,99}};
+         
+          assertArrayEquals(erotus, x.matriisienerotus(y));
+    }
+
+    /**
+     * Test of matriisitulo method, of class Normimatriisi.
+     */
+    @Test
+    public void testMatriisitulo() {
+          int[][] tulo = {{-4,-3,1},
+                         {86,27,91}};
+          
+          assertArrayEquals(tulo, z.matriisitulo(y));
+    }
+
+    /**
+     * Test of koontarkistus method, of class Normimatriisi.
+     */
+    @Test
+    public void testKoontarkistus() {
+          
+          assertTrue(x.koontarkistus(y));
+          assertFalse(y.koontarkistus(z));
+    }
+}

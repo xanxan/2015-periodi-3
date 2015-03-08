1d518ce2dda969b00ef1c2d70f268b46b4aa2c1f
xanxan <anna.emilia.salo@gmail.com>
2015-03-08 23:45:12 +0200

    dokumentointi

--- Matriisilaskin/src/main/java/superlabra/matriisilaskin/Matriisirajapinta.java
+++ Matriisilaskin/src/main/java/superlabra/matriisilaskin/Matriisirajapinta.java
@@ -2,7 +2,7 @@ package superlabra.matriisilaskin;
 
 /**
  * Matriisirajapinta määrittää ne ominaisuudet jotka ovat yhteisiä kaikille matriiseille.
- * Nämä ominaisuudet pitää löytyä kaikilta matriisiluokilta.
+ * Nämä ominaisuudet pitää löytyä kaikilta matriisiolioilta.
  * @author anna
  */
 public interface Matriisirajapinta {
